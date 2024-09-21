30a1a42ca9522d87aec3eb77c929c73782f4ed1c
이성진 <seongjin@iseongjin-ui-MacBookAir.local>
2024-09-21 22:12:52 +0900

    ..

--- docker/linux_study/run_image.sh
+++ docker/linux_study/run_image.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 docker stop linux_study
 docker rm linux_study
+#docker image rm linux_study
 docker build --tag linux_study .
 docker run --name linux_study -d linux_study:latest sleep 6000
 docker exec -it linux_study /bin/sh
