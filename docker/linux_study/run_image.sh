76493e183074e790ca452f78e423cd086929aca2
jin0118 <lsj6445z@naver.com>
2025-02-02 14:44:29 +0900

    Update run_image.sh

--- docker/linux_study/run_image.sh
+++ docker/linux_study/run_image.sh
@@ -2,5 +2,5 @@
 docker stop linux_study
 docker rm linux_study
 docker build --tag linux_study .
-docker run --name linux_study -d linux_study:latest sleep 6000
+docker run --name linux_study --restart=always -d linux_study:latest sleep 6000
 docker exec -it linux_study bash
